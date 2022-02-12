bebba38757d67f980d50fe744a138e84a94baee4
wargunnerguy <reimovellemaa@gmail.com>
2022-02-12 19:26:10 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Add some ionic markup... -->
+  <ion-page>
+    <ion-header>
+      <ion-toolbar class="toolbar-md-primary">
+        <ion-title>Ionic Nuxt App</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-card>
+      <ion-card-subtitle>Ololo</ion-card-subtitle>
+      <ion-card-title>Trololo</ion-card-title>
+      <ion-card-content>
+        <h1>Hololo!</h1>
+      </ion-card-content>
+    </ion-card>
+  </ion-page>
+</template>
